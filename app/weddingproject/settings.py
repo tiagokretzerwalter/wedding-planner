0a05f02d93a0b77476ecbe185d206544303cf75b
Gabriel <60001504+gabrielpalves@users.noreply.github.com>
2022-08-31 10:03:45 -0300

    WP-107 separated parameters
    Separated parameters DEBUG and ALLOWED_HOSTS. DEBUG was True by default, so in .env put DEBUG=True. ALLOWED_HOSTS was an empty array. You can either put ALLOWED_HOSTS = [] or = '' in .env or simply don't put a variable ALLOWED_HOSTS because the default will be its initial value [].

--- app/weddingproject/settings.py
+++ app/weddingproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=lambda v: [s.strip() for s in v.split(',')] if (v and v!='[]') else [])
 
 
 # Application definition
