7671ac8a8cfdd2e8c02fa04c5867dd18ccd20a96
Tiago Kretzer Walter <tiagokwalter@gmail.com>
2022-10-03 09:49:24 -0300

    installed drf-spectacular to document the apis

--- app/weddingproject/urls.py
+++ app/weddingproject/urls.py
@@ -13,10 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView
+)
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('weddingapp.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path(
+        'api/docs',
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs',
+    )
 ]
